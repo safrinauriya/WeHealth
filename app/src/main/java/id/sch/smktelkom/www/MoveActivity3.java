fb73737a906722173a9df12d5b8db02f2238f82b
safrinauriya <safrina__auriya_28rpl@student.smktelkom-mlg.sch.id>
2020-08-11 14:09:28 +0700

    first commit
    Signed-off-by: safrinauriya <safrina__auriya_28rpl@student.smktelkom-mlg.sch.id>

---
+++ app/src/main/java/id/sch/smktelkom/www/MoveActivity3.java
@@ -0,0 +1,14 @@
+package id.sch.smktelkom.www;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MoveActivity3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_move3);
+    }
+}
